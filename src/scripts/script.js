c433a96cf0f1d469e1ca4281509ba101de35b76c
NiKole Maxwell <nvymaxwell@outlook.com>
2017-11-09 18:41:42 -0500

    Setup File Structure

---
+++ src/scripts/script.js
@@ -0,0 +1,6 @@
+// Test for linkage 
+
+const welcome = console.log("This document repo was designed to be nvy'd by NiKole Maxwell of [nvy.io].  Please contact me for collaborations or to hire me for your next project. Check out my ongoing projects at maxnvy.github.io.");
+
+
+welcome;
